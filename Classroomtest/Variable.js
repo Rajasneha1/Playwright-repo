0c52a390e123dc7546a4494e42c35a2b221cdda7
Rajasneha1 <rajasneha.gkr@gmail.com>
2025-08-16 14:37:07 +0530

    Add files via upload

---
+++ Classroomtest/Variable.js
@@ -0,0 +1,10 @@
+let fName="play"
+console.log(typeof fName)
+let companyName="Testleaf"
+console.log(typeof companyName)
+let mobileNumber="9976534788"
+console.log(typeof mobileNumber)
+let isAutomation=true
+console.log(typeof isAutomation)
+let hasPlaywright
+console.log( typeof hasPlaywright)
